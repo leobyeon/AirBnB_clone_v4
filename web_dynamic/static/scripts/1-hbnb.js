d706732d4dd82803de89bd0406129fceb26e35be
RobertGlatzel <robertglatzel@gmail.com>
2019-02-24 22:36:05 +0000

    Adding the script for 2. It works, but I'm confused about the list. Needs review

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,21 +3,25 @@
    loaded. */
 
 document.addEventListener('DOMContentLoaded', function () {
-  let dict = {};
-
+  let list = [];
+  $('div.amenities h4').css({'width': '120%', 'height': '45%',
+			     'overflow': 'auto', 'margin-bottom': '-15px'});
   $('input').each(function(index, el) {
     let id = $(this).attr('data-id');
     let name = $(this).attr('data-name');
-
     $(el).change(function() {
       if (this.checked) {
-	dict[id] = {name: name, checked: true};
-	$('div.amenities h4').append('<span id=' + id + '>' + name +' ' + '</span>');
+	list.push(id)
+	$('div.amenities h4').append('<span id=' + id + '>'
+				     + name +'<br></span>');
       } else {
-	console.log('Unchecked');
-	dict[id] = {name: name, checked: false};
+	if (list.includes(id)){
+	  let idx = list.indexOf(id);
+	  list.splice(idx, 1);
+	}
 	$('#' + id).remove();
       }
     });
   });
+  console.log(list);
 });
